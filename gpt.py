135c4c9f8d1819297c50b509f0ab27955e6ccc0a
Brian McMillan <73612100+brian-mcmillan@users.noreply.github.com>
2023-05-06 21:28:28 -0400

    refactored new_query, added function display_playlist

--- gpt.py
+++ gpt.py
@@ -3,40 +3,41 @@ import sys
 import re
 import ast
 
-default_description = "The songs should include the most popular songs and artists of all time."
-default_length = 10
 
-def access_gpt(api_key):
-    try:
-        openai.api_key = api_key
-        return True
-    except KeyError:
-        print("Invalid API Key provided.")
-        sys.exit(1)
-
-def new_query(length=default_length, description=default_description):
+def new_query(length, description):
 
     query = f"Return a Python3 dictionary of {length} songs in the format (song:artist)." \
-            f"{description}"
+            f" {description}"
     try:
+        openai.api_key = input("Enter OpenAI API key: ")
         response = openai.ChatCompletion.create(
                     model="gpt-3.5-turbo",
                     messages=[
                         {"role": "system", "content": "You are a helpful assistant who is knowledgeable about music."},
-                        { "role": "user", "content": {query}}
+                        {"role": "user", "content": f"{query}"}
                     ]
-        )
+                )
+
         message = response['choices'][0]['message']['content']
         return message
-    except openai.error.RateLimitError:
-        print("You have exceeded OpenAI credit limit.")
+    except (openai.error.RateLimitError, openai.error.AuthenticationError) as error:
+        print(error)
         sys.exit(1)
 
 
 def parse_message(message):
-    message = message.strip("{ ", "{")
-    message = message.strip("\n", "")
+    message = message.replace("{ ", "{")
+    message = message.replace("\n", "")
     message = re.sub(' +', ' ', message)
     playlist = ast.literal_eval(re.search('({.+})', message).group(0))
 
     return playlist
+
+
+def display_playlist(playlist: dict):
+    print("\nHere is your personalized playlist :\n")
+    i = 1
+    for key, value, in playlist.items():
+        print(f"{i}.", key, "by", value)
+        i += 1
+    return
